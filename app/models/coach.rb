76526d30976cf5d8d1cce7e71706195fe305a2f2
ishthi <ishthi83@gmail.com>
2026-08-01 01:30:19 +0530

    Change coach type enum to int

--- app/models/coach.rb
+++ app/models/coach.rb
@@ -2,7 +2,7 @@ class Coach < ApplicationRecord
   belongs_to :train
   has_many :seats, dependent: :destroy
 
-  enum :coach_type, { reserved: "reserved", unreserved: "unreserved" }
+  enum :coach_type, { reserved: 0, unreserved: 1 }
 
   validates :coach_number, presence: true, uniqueness: { scope: :train_id }
   validates :seat_count, presence: true, numericality: { greater_than: 0 }
