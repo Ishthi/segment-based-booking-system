507e4120784fdc66d68e5119115d5864dd1ce2d1
ishthi <ishthi83@gmail.com>
2026-08-01 02:13:20 +0530

    Code fixes

--- app/controllers/api/v1/seats_controller.rb
+++ app/controllers/api/v1/seats_controller.rb
@@ -1,9 +1,10 @@
 class Api::V1::SeatsController < Api::V1::ApplicationController
+  before_action :set_train
+  before_action :set_coach
   before_action :set_seat, only: [ :show, :update, :destroy ]
 
   def index
-    seats = Seat.includes(:coach).order(:id)
-
+    seats = @coach.seats.includes(:coach).order(:id)
     render json: seats
   end
 
@@ -12,31 +13,35 @@ class Api::V1::SeatsController < Api::V1::ApplicationController
   end
 
   def create
-    seat = Seat.create!(seat_params)
-
+    seat = @coach.seats.create!(seat_params)
     render json: seat, status: :created
   end
 
   def update
     @seat.update!(seat_params)
-
     render json: @seat
   end
 
   def destroy
     @seat.destroy!
-
     head :no_content
   end
 
   private
 
+  def set_train
+    @train = Train.find(params[:train_id])
+  end
+
+  def set_coach
+    @coach = @train.coaches.find(params[:coach_id])
+  end
+
   def set_seat
-    @seat = Seat.find(params[:id])
+    @seat = @coach.seats.find(params[:id])
   end
 
   def seat_params
-    params.require(:seat)
-          .permit(:coach_id, :seat_number)
+    params.require(:seat).permit(:seat_number)
   end
 end
