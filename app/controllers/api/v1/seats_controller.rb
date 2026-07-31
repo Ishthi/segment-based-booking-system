5828fe4be18da1da38e459463792b79c49863231
ishthi <ishthi83@gmail.com>
2026-08-01 02:04:05 +0530

    Add codes for Seat Controller

--- app/controllers/api/v1/seats_controller.rb
+++ app/controllers/api/v1/seats_controller.rb
@@ -1,14 +1,42 @@
 class Api::V1::SeatsController < Api::V1::ApplicationController
+  before_action :set_seat, only: [ :show, :update, :destroy ]
+
+  def index
+    seats = Seat.includes(:coach).order(:id)
+
+    render json: seats
+  end
+
   def show
-    seat = Seat.find(params[:id])
-    etag = SeatVersionService.new(seat).version
+    render json: @seat
+  end
 
-    fresh_when(etag: etag, public: false)  # sets ETag header, handles If-None-Match automatically
+  def create
+    seat = Seat.create!(seat_params)
 
-    render json: {
-      id: seat.id,
-      coach: seat.coach.coach_number,
-      seat_number: seat.seat_number
-    }
+    render json: seat, status: :created
+  end
+
+  def update
+    @seat.update!(seat_params)
+
+    render json: @seat
+  end
+
+  def destroy
+    @seat.destroy!
+
+    head :no_content
+  end
+
+  private
+
+  def set_seat
+    @seat = Seat.find(params[:id])
+  end
+
+  def seat_params
+    params.require(:seat)
+          .permit(:coach_id, :seat_number)
   end
 end
