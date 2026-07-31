6d1cffcac7e46f2422e26f79a7ef320b290d6505
ishthi <ishthi83@gmail.com>
2026-08-01 00:48:43 +0530

    Add features to journey controller

--- app/controllers/api/v1/journeys_controller.rb
+++ app/controllers/api/v1/journeys_controller.rb
@@ -1,9 +1,35 @@
 class Api::V1::JourneysController < Api::V1::ApplicationController
+  before_action :set_journey, only: [ :show ]
+
+  def index
+    journeys = Journey.includes(:train).order(:travel_date)
+
+    render json: journeys
+  end
+
   def create
     journey = Journey.create!(
       journey_params
     )
 
     render json: journey
+  end
+
+  def show
+    render json: @journey
+  end
+
+  private
+
+  def set_journey
+    @journey = Journey.find(params[:id])
+  end
+
+  def journey_params
+    params.require(:journey)
+          .permit(
+            :train_id,
+            :travel_date
+          )
   end
 end
