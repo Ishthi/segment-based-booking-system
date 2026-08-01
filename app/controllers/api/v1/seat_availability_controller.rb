df280f07fdc2cb75de961c39f5d880f8d7db68f8
ishthi <ishthi83@gmail.com>
2026-08-01 18:51:16 +0530

    Fix availability controller

--- app/controllers/api/v1/seat_availability_controller.rb
+++ app/controllers/api/v1/seat_availability_controller.rb
@@ -1,16 +1,24 @@
 class Api::V1::SeatAvailabilityController < Api::V1::ApplicationController
   def show
-    journey = Journey.find(params[:journey_id])
+    train = Train.find(params[:train_id])
     origin = Station.find(params[:origin_station_id])
     destination = Station.find(params[:destination_station_id])
+    travel_date = Date.parse(params[:travel_date].to_s)
 
     seats = SeatAvailabilityService.new(
-      journey: journey, origin_station: origin, destination_station: destination
+      train_id: train.id,
+      travel_date: travel_date,
+      origin_station: origin,
+      destination_station: destination
     ).available_seats
 
     payload = seats.map do |seat|
-      { id: seat.id, coach: seat.coach.coach_number, number: seat.seat_number,
-        version: SeatVersionService.new(seat).version }
+      {
+        id: seat.id,
+        coach: seat.coach.coach_number,
+        number: seat.seat_number,
+        version: SeatVersionService.new(seat).version
+      }
     end
 
     fresh_when(etag: payload.map { |s| "#{s[:id]}:#{s[:version]}" }.join(","), public: false)
