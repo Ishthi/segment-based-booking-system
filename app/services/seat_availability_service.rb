26e63854b79c57c950d2d3f37985d1807a679da3
ishthi <ishthi83@gmail.com>
2026-08-02 17:30:57 +0530

    Fix seat availability

--- app/services/seat_availability_service.rb
+++ app/services/seat_availability_service.rb
@@ -5,14 +5,23 @@ class SeatAvailabilityService
     @range = (origin_station.sequence...destination_station.sequence)
   end
 
-  def available_seats
-    Seat.joins(coach: :train)
-        .where(coaches: { train_id: @train_id, coach_type: :reserved })
-        .where.not(
-          id: Booking.confirmed_only
-                     .where(train_id: @train_id, travel_date: @travel_date)
-                     .where("segment_range && int4range(?, ?)", @range.first, @range.last)
-                     .select(:seat_id)
-        )
+  def coach_availability
+    Coach.where(train_id: @train_id).includes(:seats).map do |coach|
+      booked_seat_ids = Booking.confirmed_only
+        .where(train_id: @train_id, travel_date: @travel_date)
+        .where("segment_range && int4range(?, ?)", @range.first, @range.last)
+        .where(seat_id: coach.seat_ids)
+        .pluck(:seat_id)
+
+      available_seats = coach.seats.where.not(id: booked_seat_ids)
+
+      {
+        coach_id: coach.id,
+        coach_number: coach.coach_number,
+        coach_type: coach.coach_type,
+        available_seat_count: available_seats.count,
+        seats: available_seats
+      }
+    end
   end
 end
