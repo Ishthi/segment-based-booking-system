887932aa0698cd9eda1e793447365bf5cbe3e23b
ishthi <ishthi83@gmail.com>
2026-08-02 17:59:04 +0530

    Add fare for reserved and unreserved

--- app/services/fare_calculator_service.rb
+++ app/services/fare_calculator_service.rb
@@ -1,13 +1,21 @@
 class FareCalculatorService
   RATE_PER_KM = 3.2
+  RATE_PER_KM_UNRESERVED = 2.0
 
-  def initialize(origin_station, destination_station)
+  def initialize(origin_station, destination_station, coach_type = "reserved")
     @origin = origin_station
     @destination = destination_station
+    @coach_type = coach_type.to_s
   end
 
   def calculate
     distance_km = @destination.distance_km - @origin.distance_km
-    (distance_km * RATE_PER_KM).round
+    (distance_km * rate_per_km).round
+  end
+
+  private
+
+  def rate_per_km
+    @coach_type == "unreserved" ? RATE_PER_KM_UNRESERVED : RATE_PER_KM
   end
 end
